d52994579165d00b02f3bf84f02381c229719742
vidyav <vidya@qburst.com>
2025-08-22 18:02:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ echo "Test displayed";
 header("Content-Type: application/json");
 
 $input = json_decode(file_get_contents('php://input'), true);
-print_r($input);
+print_r($_REQUEST);
 //file_put_contents('php://stderr', print_r($input, true)); // Debug logging
 
 // Respond back
