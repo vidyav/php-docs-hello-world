c9610294c01f13f7c30f849f7b33a66c26ef47f0
vidyav <vidya@qburst.com>
2025-08-22 16:50:40 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,3 +4,11 @@ echo "Test displayed";
 
 $test = $_POST;
 print_r($_POST);
+
+header("Content-Type: application/json");
+
+$input = json_decode(file_get_contents('php://input'), true);
+file_put_contents('php://stderr', print_r($input, true)); // Debug logging
+
+// Respond back
+echo json_encode(['status' => 'received']);
