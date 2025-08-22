37f259d087cf39b0368b2bfcd4e12e6ba61b8e29
vidyav <vidya@qburst.com>
2025-08-22 16:55:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,8 @@ print_r($_POST);
 header("Content-Type: application/json");
 
 $input = json_decode(file_get_contents('php://input'), true);
-file_put_contents('php://stderr', print_r($input, true)); // Debug logging
+print_r($input, true);
+//file_put_contents('php://stderr', print_r($input, true)); // Debug logging
 
 // Respond back
 echo json_encode(['status' => 'received']);
